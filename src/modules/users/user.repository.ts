d85c78f6403433c830309bf2e4444d398b6c3640
imranmalakzai <afghanimran92@gmail.com>
2026-01-29 15:05:45 +0430

    feat(users): add change password interface type

--- src/modules/users/user.repository.ts
+++ src/modules/users/user.repository.ts
@@ -1,12 +1,6 @@
 import { pool } from "../../infra/mysql";
 import { ResultSetHeader } from "mysql2";
-
-//user preference
-interface person {
-  name: string;
-  email: string;
-  password: string;
-}
+import { person, password as ps } from "./user.types";
 
 // Register a new user
 export const register = async (user: person): Promise<number> => {
@@ -15,4 +9,13 @@ export const register = async (user: person): Promise<number> => {
     [user.name, user.email, user.password],
   );
   return result.insertId || 0;
+};
+
+//update password
+export const password = async (data: ps): Promise<number> => {
+  const [result] = await pool.query<ResultSetHeader>(
+    "UPDATE users SET password = ? WHERE id = ?",
+    [data.id, data.password],
+  );
+  return result.insertId;
 };
