96aa5c00f64ae98becd7f48dd1d161f413d34722
imranmalakzai <afghanimran92@gmail.com>
2026-01-29 12:22:44 +0430

    fix(env): add requred varibles inside env file

--- src/infra/env.ts
+++ src/infra/env.ts
@@ -2,4 +2,14 @@ import env from "dotenv";
 
 env.config({ path: ".env.production" });
 
-export const { name } = process.env;
+export const {
+  HOST,
+  USER,
+  PASSWORD,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_EXPIRY,
+  CORS_ORIGIN,
+  REDIS_SERVER_URL,
+} = process.env;
