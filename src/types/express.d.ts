195f1687cf8e0b2d6488043c0861d42b573d65c2
imranmalakzai <afghanimran92@gmail.com>
2026-01-30 12:26:38 +0430

    fix : global user type optional chine issue

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,7 +3,7 @@ import { Request } from "express";
 declare global {
   namespace Express {
     interface Request {
-      user?: { id: number; role: "admin" | "owner" | "user" };
+      user: { id: number; role: "admin" | "owner" | "user" };
     }
   }
 }
