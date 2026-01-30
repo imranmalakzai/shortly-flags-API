fefc081595c49134c88b1d86c5bc0a8913919ef4
imranmalakzai <afghanimran92@gmail.com>
2026-01-30 11:59:16 +0430

    fix : correct authentication middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,17 +1,25 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
 import jwt from "jsonwebtoken";
 import * as env from "../config/env";
 import ApiError from "../utils/apiError";
 
-const auth = async (req: Request, res: Response) => {
+interface AuthPayload extends JwtPayload {
+  id: number;
+  role: "admin" | "user" | "owner";
+}
+
+const auth = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const header = req.headers.authorization;
     if (!header || !header.startsWith("Bearer")) {
       throw new ApiError("please authenticate", 401);
     }
-    const token = await header.split(" ")[1];
-    const user = await jwt.verify(token, env.ACCESS_TOKEN_SECRET!);
-    return user;
+    const token = header.split(" ")[1];
+    const payload = jwt.verify(token, env.ACCESS_TOKEN_SECRET!) as AuthPayload;
+
+    req.user = payload;
+    next();
   } catch (error) {
     throw new ApiError("Invalid token or token is expired", 401);
   }
