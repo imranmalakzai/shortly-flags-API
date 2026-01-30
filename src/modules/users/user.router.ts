9ed154ba06db9e043fd64603fc47afb12a6d2a6f
imranmalakzai <afghanimran92@gmail.com>
2026-01-30 15:09:22 +0430

    feat(users): add users api route endpoints

--- src/modules/users/user.router.ts
+++ src/modules/users/user.router.ts
@@ -7,6 +7,9 @@ const userRouter = express.Router();
 userRouter.route("/auth/register").post(users.register);
 userRouter.route("/auth/login").post(auth, users.login);
 
+userRouter.route("/users").get(auth, users.users);
+userRouter.route("/users/:userId").get(users.user);
+
 userRouter.route("/users/me").get(auth, users.me);
 userRouter.route("/users/me").delete(auth, users.remove);
 userRouter.route("/users/me/password").patch(auth, users.password);
