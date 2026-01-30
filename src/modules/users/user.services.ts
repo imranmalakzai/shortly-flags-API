bc9ca92a57a61af2bc99812c6c3790e59b66e69f
imranmalakzai <afghanimran92@gmail.com>
2026-01-30 06:51:47 +0430

    feat(auth): create login services

--- src/modules/users/user.services.ts
+++ src/modules/users/user.services.ts
@@ -1,8 +1,8 @@
 import ApiError from "../../utils/apiError";
 import * as db from "./user.repository";
 import bcrypt from "bcrypt";
-
 import { person } from "./user.types";
+import { accessToken, refreshToken } from "../../infra/jwt";
 
 export const register = async (data: person): Promise<number> => {
   // user already exist
@@ -19,4 +19,21 @@ export const register = async (data: person): Promise<number> => {
     email: data.email,
   });
   return result;
+};
+
+export const login = async (data: { email: string; password: string }) => {
+  //user exist
+  const user = await db.getUserByEmail(data.email);
+  if (!user) throw new ApiError("Invalid cridential", 403);
+
+  //match password
+  const isMatch = await bcrypt.compare(data.password, user.password);
+  if (!isMatch) throw new ApiError("Invalid cridential", 403);
+
+  // tokens
+  const access_token = await accessToken(user);
+  const refresh_oken = await refreshToken(user);
+
+  //add redis later here!! to handle refreshToken
+  return { user, accessToken, refresh_oken };
 };
