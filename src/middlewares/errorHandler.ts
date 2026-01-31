197473aadc26e669b42869e1945940d8a754af5e
imranmalakzai <afghanimran92@gmail.com>
2026-01-31 11:23:43 +0430

    feat(validation): add zod validation error handler inside global error handler middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,3 +1,5 @@
+import { ZodError } from "zod";
+import { zodErrorHandler } from "../infra/zod";
 import ApiError from "../utils/apiError";
 import { Request, Response, NextFunction } from "express";
 
@@ -18,6 +20,13 @@ const errorHandler = (
     // if err belongs to Error class
     if (err instanceof Error) {
       return res.status(500).json({ message: err.message, success: false });
+    }
+
+    if (err instanceof ZodError) {
+      const apiError = zodErrorHandler(err);
+      return res
+        .status(apiError.statusCode)
+        .json({ message: apiError.message, success: apiError.success });
     }
 
     //unspected error
