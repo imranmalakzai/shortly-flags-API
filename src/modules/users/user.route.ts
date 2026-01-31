4b92ed2721cbe70234c3eae99697862703a5916f
imranmalakzai <afghanimran92@gmail.com>
2026-01-31 17:18:05 +0430

    feat(docs): create proper comment the user route api end points

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -7,23 +7,37 @@ import validate from "../../middlewares/validate";
 
 const userRouter = express.Router();
 
+//register
 userRouter
   .route("/auth/register")
   .post(validate(schema.register), users.register);
 
+// login
 userRouter.route("/auth/login").post(validate(schema.login), users.login);
 
+// fetch all users
 userRouter.route("/users").get(auth, users.users);
+
+// fetch a user by userId
 userRouter.route("/users/:userId").get(users.user);
 
+// get currect logged profile
 userRouter.route("/users/me").get(auth, users.me);
-userRouter.route("/users/me").delete(auth, users.remove);
-userRouter.route("/users/me/password").patch(auth, users.password);
 
+// delete current account
+userRouter.route("/users/me").delete(auth, users.remove);
+
+// update password
+userRouter
+  .route("/users/me/password")
+  .patch(auth, validate(schema.password), users.password);
+
+// delete user account
 userRouter
   .route("/admin/users/:userId")
   .patch(auth, validRole("owner"), users.updateRole);
 
+// update user role
 userRouter
   .route("/admin/users/:userId")
   .delete(auth, validRole("owner", "admin"), users.deleteUser);
