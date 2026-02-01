a82eecc62b9415a4e6533c2bc71f833b8358d8c2
imranmalakzai <afghanimran92@gmail.com>
2026-02-01 20:15:55 +0430

    feat(urls): add create a user short and long urls

---
+++ src/modules/urls/url.repository.ts
@@ -0,0 +1,12 @@
+import { pool } from "../../infra/mysql";
+import { ResultSetHeader } from "mysql2";
+import { url } from "./url.type";
+
+// Add long and short url of a user
+export const create = async (data: url) => {
+  const [result] = await pool.query<ResultSetHeader>(
+    "INSERT INTO urls (user_id,original_url,short_code) VALUES (?,?,?)",
+    [data.user_id, data.original_url, data.short_code],
+  );
+  return result.insertId;
+};
