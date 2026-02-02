fdb5949deaba5fd3bea496237b65283aa9c750e7
imranmalakzai <afghanimran92@gmail.com>
2026-02-02 22:14:47 +0430

    fix : genereate short url database query

--- src/modules/urls/url.repository.ts
+++ src/modules/urls/url.repository.ts
@@ -3,7 +3,11 @@ import { ResultSetHeader } from "mysql2";
 import { url } from "./url.type";
 
 // Add long and short url of a user
-export const create = async (data: url) => {
+export const create = async (data: {
+  user_id?: number | null;
+  original_url: string;
+  short_code: string;
+}) => {
   const [result] = await pool.query<ResultSetHeader>(
     "INSERT INTO urls (user_id,original_url,short_code) VALUES (?,?,?)",
     [data.user_id || null, data.original_url, data.short_code],
