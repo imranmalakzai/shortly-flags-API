05d609c1700c02d8e7ce94f724c878660482bf3e
imranmalakzai <afghanimran92@gmail.com>
2026-02-03 20:30:11 +0430

    feat(soft auth): add soft auth in url api route endpoints

--- src/modules/urls/url.route.ts
+++ src/modules/urls/url.route.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import * as url from "./url.controller";
-import auth from "../../middlewares/auth";
+import softAuth from "../../middlewares/softAuth";
 import * as schema from "./url.schema";
 import validate from "../../middlewares/validate";
 
 const urlRouter = express.Router();
 
-urlRouter.route("/").post(auth, validate(schema.create), url.create);
-urlRouter.route("/:shortCode").get(auth, url.oringalUrl);
+urlRouter.route("/").post(softAuth, validate(schema.create), url.create);
+urlRouter.route("/:shortCode").get(softAuth, url.oringalURL);
 
 export default urlRouter;
