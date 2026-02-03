c0760650e7e2d6e50ef5adc6a6343374b2cfa9d3
imranmalakzai <afghanimran92@gmail.com>
2026-02-03 20:24:56 +0430

    feat(sortAuth) : create a soft auth middlewares   1: it will help guest to generate short url without loggin   2: it will redirect user to oringinal url without checking for req.ip

---
+++ src/middlewares/softAuth.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import * as env from "../config/env";
+import ApiError from "../utils/apiError";
+
+interface AuthPayload extends JwtPayload {
+  id: number;
+  role: "admin" | "user" | "owner";
+}
+
+const softAuth = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const header = req.headers.authorization;
+    if (!header) {
+      return next(); //loggin as a gest
+    }
+    const token = header.split(" ")[1];
+    const payload = jwt.verify(token, env.ACCESS_TOKEN_SECRET!) as AuthPayload;
+
+    req.user = payload;
+    next();
+  } catch (error) {
+    throw new ApiError("Invalid token or token is expired", 401);
+  }
+};
+
+export default softAuth;
