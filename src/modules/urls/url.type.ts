145aead1a30ee31cd21b5631ac439acfc6b53d6a
imranmalakzai <afghanimran92@gmail.com>
2026-02-03 08:39:24 +0430

    fix: correct url typo

--- src/modules/urls/url.type.ts
+++ src/modules/urls/url.type.ts
@@ -1,5 +1,6 @@
-export interface url {
+import { RowDataPacket } from "mysql2";
+export interface url extends RowDataPacket {
   user_id?: number;
   original_url: string;
-  // short_code: string;
+  short_code?: string;
 }
