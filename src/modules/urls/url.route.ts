778635aa5cf6e36fa76ced31500c0e38af5cf0cb
imranmalakzai <afghanimran92@gmail.com>
2026-02-04 08:05:13 +0430

    feat(url): add delete url api end point

--- src/modules/urls/url.route.ts
+++ src/modules/urls/url.route.ts
@@ -3,10 +3,13 @@ import * as url from "./url.controller";
 import softAuth from "../../middlewares/softAuth";
 import * as schema from "./url.schema";
 import validate from "../../middlewares/validate";
+import auth from "../../middlewares/auth";
+import { validRole } from "../../middlewares/validRole";
 
 const urlRouter = express.Router();
 
 urlRouter.route("/").post(softAuth, validate(schema.create), url.create);
 urlRouter.route("/:shortCode").get(softAuth, url.oringalURL);
+urlRouter.route("/urlId").delete(auth, validRole("admin"), url.deleteUrl);
 
-export default urlRouter;
+export default url;
