c13010b02d61d7a8482c95e6ed68437e37bf2e48
imranmalakzai <afghanimran92@gmail.com>
2026-02-06 14:58:53 +0430

    refct : optimize url route endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { connectRedis } from "./infra/redis";
+import urlRouter from "./modules/urls/url.route";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/", urlRouter);
 
 const startServer = async () => {
   await connectRedis();
